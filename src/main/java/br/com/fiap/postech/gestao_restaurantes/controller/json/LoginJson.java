9532e5809b3eade89356c5d2c4ca0a544dd8269e
Felipe Matos <Felipe_Matos@Hotmail.com.br>
2025-06-01 20:24:37 -0300

    #6 feat: Documentação do projeto e criação da função de documentação

--- src/main/java/br/com/fiap/postech/gestao_restaurantes/controller/json/LoginJson.java
+++ src/main/java/br/com/fiap/postech/gestao_restaurantes/controller/json/LoginJson.java
@@ -2,6 +2,7 @@ package br.com.fiap.postech.gestao_restaurantes.controller.json;
 
 
 import br.com.fiap.postech.gestao_restaurantes.domain.Credenciais;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,12 +11,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Dados para autenticação de usuário")
 public class LoginJson {
 
     @NotBlank
+    @Schema(description = "Login ou email do usuário", example = "usuario@exemplo.com", required = true)
     private String login;
 
     @NotBlank
+    @Schema(description = "Senha do usuário", example = "senhaSegura123", required = true)
     private String senha;
 
     public Credenciais mapToDomain(){
--- src/main/java/br/com/fiap/postech/gestao_restaurantes/controller/json/UsuarioJson.java
+++ src/main/java/br/com/fiap/postech/gestao_restaurantes/controller/json/UsuarioJson.java
@@ -2,6 +2,7 @@ package br.com.fiap.postech.gestao_restaurantes.controller.json;
 
 
 import br.com.fiap.postech.gestao_restaurantes.domain.Usuario;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,24 +13,33 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Representação de um usuário")
 public class UsuarioJson {
+
+    @Schema(description = "ID do usuário", example = "1")
     private Long id;
 
     @NotBlank
+    @Schema(description = "CPF do usuário", example = "123.456.789-00", required = true)
     private String cpf;
 
     @NotBlank
+    @Schema(description = "Nome completo", example = "João da Silva", required = true)
     private String nome;
 
     @NotBlank
+    @Schema(description = "Email", example = "joao.silva@email.com", required = true)
     private String email;
 
     @NotBlank
+    @Schema(description = "Login de acesso", example = "joaosilva", required = true)
     private String login;
 
     @NotBlank
+    @Schema(description = "Senha de acesso", example = "senhaForte123", required = true)
     private String senha;
 
+    @Schema(description = "Endereço do usuário")
     private EnderecoJson endereco;
 
     public Usuario mapToDomain(){
