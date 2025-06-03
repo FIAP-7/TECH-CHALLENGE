93f5d91d9ac21b46a6b0fa808e65564c8fbeb117
Michael Barroso <michael_p._b.15600@hotmail.com>
2025-06-02 21:26:23 -0300

    #12 fix: Correção da mensagem referente a mensagem de erro referente ao padrão de senha.

--- src/main/java/br/com/fiap/postech/gestao_restaurantes/exception/SenhaFormatoInvalidoException.java
+++ src/main/java/br/com/fiap/postech/gestao_restaurantes/exception/SenhaFormatoInvalidoException.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 public class SenhaFormatoInvalidoException extends SystemBaseException {
 
     private final String code = "usuario.senhaFormatoInvalido";
-    private final String message = "Senha precisa ter mais de 8 caracteres e possuir pelo menos uma letra, um numero e um caracter especial.";
+    private final String message = "Senha precisa ter mais de 8 caracteres e possuir pelo menos uma letra maiúscula, uma letra minúscula, um número e um caracter especial.";
     private final Integer httpStatus = 401;
 }
