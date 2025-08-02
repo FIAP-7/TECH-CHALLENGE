3b75762850214f4abcafc788c75f275ce5376de5
Gustavo Leite <gustavo.jog@hotmail.com>
2025-08-02 18:59:48 -0300

    fix: Ajusta formatação do preço do item

--- src/main/java/br/com/fiap/postech/gestao_restaurantes/core/presenters/ItemCardapioPresenter.java
+++ src/main/java/br/com/fiap/postech/gestao_restaurantes/core/presenters/ItemCardapioPresenter.java
@@ -4,10 +4,33 @@ import br.com.fiap.postech.gestao_restaurantes.core.dto.NovoItemCardapioDTO;
 import br.com.fiap.postech.gestao_restaurantes.core.dto.ItemCardapioDTO;
 import br.com.fiap.postech.gestao_restaurantes.core.entities.ItemCardapio;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class ItemCardapioPresenter {
 
+    private static final DecimalFormat DECIMAL_FORMAT;
+
+    static {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        symbols.setDecimalSeparator('.');
+        DECIMAL_FORMAT = new DecimalFormat("#.00", symbols);
+    }
+
     public static ItemCardapioDTO toDTO(ItemCardapio itemCardapio) {
-        return new ItemCardapioDTO(itemCardapio.getId(), itemCardapio.getNome(), itemCardapio.getDescricao(), itemCardapio.getPreco(), itemCardapio.isDisponivelApenasNoRestaurante(), itemCardapio.getFoto());
+        String precoFormatado = DECIMAL_FORMAT.format(itemCardapio.getPreco());
+        BigDecimal preco = new BigDecimal(precoFormatado);
+
+        return new ItemCardapioDTO(
+                itemCardapio.getId(),
+                itemCardapio.getNome(),
+                itemCardapio.getDescricao(),
+                preco,
+                itemCardapio.isDisponivelApenasNoRestaurante(),
+                itemCardapio.getFoto()
+        );
     }
 
     public static NovoItemCardapioDTO toNovoTipoDTO(ItemCardapio itemCardapio) {
